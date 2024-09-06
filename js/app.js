9070b908fc16918a6c1e082affd3815c7cd3e86d
leonardojgm <cookie.maia@gmail.com>
2024-09-06 08:05:28 -0300

    Melhorias de opção de navegação

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const resultadosPesquisa = document.getElementById('resultados-pesquisa');
 const campoPesquisa = document.getElementById('campo-pesquisa');
 const botaoPesquisa = document.getElementById('botao-pesquisa');
+const acoes = document.getElementById('acoes');
+let historia = "";
 
 campoPesquisa.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
@@ -10,27 +12,104 @@ campoPesquisa.addEventListener('keydown', (event) => {
 });
 
 botaoPesquisa.addEventListener('click', () => {
-  exibirPassoAPasso();
+  criarAventura();
 });
 
-async function exibirPassoAPasso() {
-  const itemResultado = document.createElement('div');
-
+async function criarAventura() {
   document.getElementById('loader').style.display = 'block';
 
+  campoPesquisa.remove();
+  botaoPesquisa.remove();
+
+  await criarHistoria();
+}
+
+async function avancarAventura(resposta) {
+  resultadosPesquisa.innerHTML = "";
+  acoes.innerHTML = "";
+  document.getElementById('main').classList.add('centro');
+  document.getElementById('loader').style.display = 'block';
+
+  await avancarHistoria(resposta);
+}
+
+async function criarHistoria() {
   // Construindo o prompt
   let prompt = `
+    A resposta tem que vir formatada para HTML.
     Você é o mestre de uma aventura de RPG, a aventura começa assim:
     ${campoPesquisa.value}.
     Você precisa pegar essa narrativa e introduzir a história.
-    Na sequencia farei outra solicitação pedindo 4 opções para tomada de decisão do jogador.
-    A resposta tem que vir formatada para HTML.`;
+    O ponto de partida da aventura sempre é a Taverna, mas a palavra Taverna não pode ser usada no título.
+    A aventura deve possuir um objetivo claro na introdução.
+    Na sequencia exiba 4 opções usando tag <table></table> para tomada de decisão do jogador, exemplo:
+    <table>
+      <tbody>
+      <tr>
+        <td>A) Descrição.</td>
+      </tr>
+      <tr>
+        <td>B) Descrição.</td>
+      </tr>
+      <tr>
+        <td>C) Descrição.</td>
+      </tr>
+      <tr>
+        <td>D) Descrição.</td>
+      </tr>
+    </tbody></table>`;
+  
+  // Criando o objeto a ser enviado no corpo da requisição
+  const body = {
+    prompt: prompt
+  };  
+
+  const texto = await consultarGemini(body)
+
+  await atualizarResultado(texto);
+}
+
+async function avancarHistoria(resposta) {
+  // Construindo o prompt
+  let prompt = `
+    Continue a história do ponto onde parou: 
+    "${historia}".
+    O usuario escolheu a opção ${resposta}.
+    Você deve incluir no final do texto a frase "FIM DE JOGO" caso o jogador tenha chegado a uma das situações:
+    1. Morreu
+    2. Concluiu seu objetivo principal
+    3. Venceu o jogo
+    4. Derrotou o chefão final
+    5. Se perdeu
+    6. O vilão alcançou seu objetivo
+    Se não você deve exibir novas 4 opções usando tag <table></table> para tomada de decisão do jogador, exemplo:
+    <table>
+      <tbody>
+      <tr>
+        <td>A) Descrição.</td>
+      </tr>
+      <tr>
+        <td>B) Descrição.</td>
+      </tr>
+      <tr>
+        <td>C) Descrição.</td>
+      </tr>
+      <tr>
+        <td>D) Descrição.</td>
+      </tr>
+    </tbody></table>`;
   
   // Criando o objeto a ser enviado no corpo da requisição
   const body = {
     prompt: prompt
   };
 
+  const texto = await consultarGemini(body);
+
+  await atualizarResultado(texto);
+}  
+
+async function consultarGemini(body) {
   try {
     // Fazendo a requisição POST
     const response = await fetch('https://mini-integracao-api-gemini.vercel.app/', {
@@ -45,29 +124,97 @@ async function exibirPassoAPasso() {
       throw new Error(`Erro ao fazer a requisição: ${response.statusText}`);
     }
 
-    const inicio = await response.json();
+    const resposta = await response.json();
 
-    document.getElementById('loader').style.display = 'none';
+    if (!resposta || !resposta.length || resposta.length === 0 || resposta === "") {
+      throw new Error('Resposta vazia');
+    }
 
-    itemResultado.innerHTML = `
-    <div>
-      ${inicio}
-    </div>`;
-
-    itemResultado.classList.add('item-resultado');
-
-    document.getElementById('main').classList.remove('centro');
-    
-    campoPesquisa.style.display = 'none';
-  
-    resultadosPesquisa.appendChild(itemResultado);
-
-    botaoPesquisa.remove();
+    return resposta;
   } catch (error) {
     console.error('Erro:', error);
 
     await new Promise(resolve => setTimeout(resolve, 10000));
 
-    return exibirPassoAPasso();
+    return atualizarResultado();
   }
+}
+
+async function atualizarResultado(texto) {
+  const itemResultado = document.createElement('div');
+  
+  document.getElementById('main').classList.remove('centro');
+  document.getElementById('loader').style.display = 'none';
+
+  historia = texto;
+
+  itemResultado.innerHTML = `
+  <div>
+    ${texto}
+  </div>`;
+
+  itemResultado.classList.add('item-resultado');
+
+  resultadosPesquisa.appendChild(itemResultado);
+
+  if (!texto.includes("FIM DE JOGO")) {
+    await exibirBotoesOpcoes()
+  }
+  else {    
+    await exibirBotaoReiniciar()
+  }
+}
+
+async function exibirBotoesOpcoes() {
+  const buttonA = document.createElement('button');
+
+  buttonA.id = 'button-a';
+  buttonA.innerHTML = 'A';
+  buttonA.addEventListener('click', function() { 
+    avancarAventura(`A`);
+  });
+
+  acoes.appendChild(buttonA);
+  
+  const buttonB = document.createElement('button');
+
+  buttonB.id = 'button-b';
+  buttonB.innerHTML = 'B';
+  buttonB.addEventListener('click', function() { 
+    avancarAventura(`B`);
+  });
+
+  acoes.appendChild(buttonB);
+  
+  const buttonC = document.createElement('button');
+
+  buttonC.id = 'button-c';
+  buttonC.innerHTML = 'C';
+  buttonC.addEventListener('click', function() { 
+    avancarAventura(`C`);
+  });
+
+  acoes.appendChild(buttonC);
+
+  const buttonD = document.createElement('button');
+
+  buttonD.id = 'button-d';
+  buttonD.innerHTML = 'D';
+  buttonD.addEventListener('click', function() { 
+    avancarAventura(`D`);
+  });
+
+  acoes.appendChild(buttonD);
+}
+
+async function exibirBotaoReiniciar() {
+  const reiniciar = document.createElement('button');
+
+  reiniciar.id = 'reiniciar';
+  reiniciar.innerHTML = 'Recomeçar!';
+  reiniciar.addEventListener('click', function() { 
+      window.location.reload();
+  });
+
+  acoes.appendChild(reiniciar);
 }
